af8ace083524277fdec5826e184e853d068ae07d
samuelchen4 <103549382+samuelchen4@users.noreply.github.com>
2022-08-24 18:04:17 -0600

    Used prop from paymentOwed

--- src/LessonsTable.js
+++ src/LessonsTable.js
@@ -2,27 +2,28 @@ import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import { motion } from 'framer-motion';
 
-const LessonsTable = (studentInfo) => {
+const LessonsTable = (overdueLessonsInfo) => {
   const domain = 'http://localhost:5000';
   //use student info to get lessons owed
+  const {overdueLessonsInfo} = overdueLessonsInfo
   const { user_id, fn, ln, phone, email, dob } = studentInfo;
   //slice dob .slice[0,10]
   // const entries = Object.entries(lessons);
-  const [lessons, setLessons] = useState([]);
+//   const [lessons, setLessons] = useState([]);
 
-  useEffect(() => {
-    Axios.get(`${domain}/tracker/user/lessons`, {
-      params: {
-        user_id,
-      },
-    })
-      .then((res) => {
-        // console.log(res.data);
-        setLessons(res.data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-  console.log(lessons);
+//   useEffect(() => {
+//     Axios.get(`${domain}/tracker/user/lessons`, {
+//       params: {
+//         user_id,
+//       },
+//     })
+//       .then((res) => {
+//         // console.log(res.data);
+//         setLessons(res.data);
+//       })
+//       .catch((err) => console.log(err));
+//   }, []);
+//   console.log(lessons);
   return (
     <motion.div
       animate={{ maxHeight: 400, opacity: 1 }}
@@ -38,7 +39,7 @@ const LessonsTable = (studentInfo) => {
           </tr>
         </thead>
         <tbody>
-          {lessons.map((lesson) => {
+          {overdueLessonsInfo.map((lesson) => {
             return (
               <tr key={lesson.type_id}>
                 <td>{lesson.type_name}</td>
@@ -52,8 +53,8 @@ const LessonsTable = (studentInfo) => {
   );
 };
 
-LessonsTable.defaultProps = {
-  lessons: {},
-};
+// LessonsTable.defaultProps = {
+//   lessons: {},
+// };
 
 export default LessonsTable;

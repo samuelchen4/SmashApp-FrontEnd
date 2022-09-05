a956104a7d4fcb08195359f1a018806cc6eb2e36
samuelchen4 <sam_chen4@outlook.com>
2022-09-05 00:25:22 -0600

    changing css

--- src/Agenda.js
+++ src/Agenda.js
@@ -216,12 +216,18 @@ const Agenda = () => {
               placeholder='search...'
               value={search}
               onChange={searchLesson}
+              className='custom-input'
             />
             <i class='bx bx-search-alt'></i>
           </form>
           <div className='sort-by'>
             <p>sort by: </p>
-            <select name='sortby' id='sortby' onChange={sort}>
+            <select
+              name='sortby'
+              id='sortby'
+              onChange={sort}
+              className='custom-select'
+            >
               <option value='all'>all</option>
               <option value='group'>group</option>
               <option value='private'>private</option>
@@ -233,6 +239,7 @@ const Agenda = () => {
           <AgendaDatePicker
             wrapperClassName='datePicker'
             setSelectedDate={setSelectedDate}
+            className='custom-select'
           />
         </section>
       </header>

9562a6a6c0b78a3a0b5b91ca63cdc7841b68e778
samuelchen4 <sam_chen4@outlook.com>
2022-09-08 21:44:27 -0600

    debugging

--- src/PurchaseLessons.js
+++ src/PurchaseLessons.js
@@ -98,7 +98,7 @@ const PurchaseLessons = (propsFromUser) => {
     //post a puchase for this user, make puchase paid and deduct from the paymentTotal
     //post purchases for partners if nessacary, make unpaid
     //do http request first
-    // Axios.post(`${domain}/`);
+    Axios.post(`${domain}/user/${userId}/purchase`);
   };
 
   return (

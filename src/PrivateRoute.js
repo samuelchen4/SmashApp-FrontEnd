d197e45e1f6e9049f8e6399707a50d7f9d1f42cf
samuelchen4 <sam_chen4@outlook.com>
2022-09-23 02:09:34 -0600

    added login page

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const PrivateRoute = ({ children }) => {
   const { user, isAuthenticated } = useAuth0();
 
-  return isAuthenticated ? children : <Navigate to='/login' />;
+  useEffect(() => {
+    if (isAuthenticated == false) {
+      <Navigate to='/login' />;
+    }
+  }, [isAuthenticated]);
+
+  //   return isAuthenticated ? children : <Navigate to='/login' />;
+  return children;
 };
 
 export default PrivateRoute;

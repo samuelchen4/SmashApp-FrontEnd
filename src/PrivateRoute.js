538fa2d2c2ec84284f191beece38e05e31ad127c
samuelchen4 <sam_chen4@outlook.com>
2022-09-23 13:58:02 -0600

    added login page

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -6,27 +6,47 @@ import { useAuth0 } from '@auth0/auth0-react';
 const PrivateRoute = ({ children }) => {
   const { user, isAuthenticated } = useAuth0();
 
-  const navigate = useNavigate();
-  const routeChange = () => {
-    const path = '/login';
-    navigate(path);
-  };
+  const [isLoading, setIsLoading] = useState(true);
 
-  if (!isAuthenticated) {
-    return (
-      <>
-        <div>
-          <h4>Redirect to login:</h4>
-          <button onClick={routeChange}>Redirect</button>
-        </div>
-      </>
-    );
-  } else {
+  useEffect(() => {
+    //loading screen
+    console.log(isLoading);
+    console.log(isAuthenticated);
+    console.log(`changed authenticated`);
+  }, [isAuthenticated]);
+
+  //   const changeLoading = setTimeout(setIsLoading(false), '1000');
+
+  const navigate = useNavigate();
+  //   const routeChange = () => {
+  //     const path = '/login';
+  //     navigate(path);
+  //   };
+
+  if (!isLoading && isAuthenticated) {
     return children;
+  } else if (!isLoading && !isAuthenticated) {
+    navigate('/login');
+  } else {
+    <h1>Is Loading</h1>;
   }
+
+  //   if (!isAuthenticated) {
+  //     return (
+  //       <>
+  //         <div>
+  //           <h4>Redirect to login:</h4>
+  //           <button onClick={routeChange}>Redirect</button>
+  //         </div>
+  //       </>
+  //     );
+  //   } else {
+  //     return children;
+  //   }
   //   setTimeout(() => {
   //     return isAuthenticated ? children : <Navigate to='/login' />;
   //   }, '1000');
+  return children;
 };
 
 export default PrivateRoute;

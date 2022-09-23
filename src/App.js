328ad3ac6d003e0b636e2377b2dbaaaed68e86c1
samuelchen4 <sam_chen4@outlook.com>
2022-09-23 01:53:09 -0600

    added login page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Navbar from './Navbar';
 import {
   BrowserRouter as Router,
@@ -5,6 +6,7 @@ import {
   Routes,
   Switch,
 } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 
 //Routes
 import Main from './Main';
@@ -12,19 +14,50 @@ import LoginPage from './LoginPage';
 import Users from './Users';
 import User from './User';
 import Lessons from './Lessons';
+import PrivateRoute from './PrivateRoute';
+import Private from './Private';
 
 // import Calendar from './AgendaCalendar';
+const { user, isAuthenticated } = useAuth0();
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Main />} />
+        <Route
+          path='/'
+          element={
+            <PrivateRoute isAuthenticated={isAuthenticated}>
+              <Main />
+            </PrivateRoute>
+          }
+        />
         <Route path='/login' element={<LoginPage />} />
-        <Route path='/users' element={<Users />} />
+        <Route
+          path='/users'
+          element={
+            <PrivateRoute isAuthenticated={isAuthenticated}>
+              <Users />
+            </PrivateRoute>
+          }
+        />
 
-        <Route path='/lessons' element={<Lessons />} />
-        <Route path='/user/:id' element={<User />} />
+        <Route
+          path='/lessons'
+          element={
+            <PrivateRoute isAuthenticated={isAuthenticated}>
+              <Lessons />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='/user/:id'
+          element={
+            <PrivateRoute isAuthenticated={isAuthenticated}>
+              <User />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </Router>
   );
